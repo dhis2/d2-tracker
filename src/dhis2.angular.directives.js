0b5af8b4cb52e2311795fbb7e9499613fc4cf275
Lukas Wijgaart van Dijk <lukasewvd@gmail.com>
2018-10-19 15:41:17 +0200

    New validation strategy (#26)
    * Fixed required error message for radio buttons
    
    * Null check

--- src/dhis2.angular.directives.js
+++ src/dhis2.angular.directives.js
@@ -697,7 +697,8 @@ var d2Directives = angular.module('d2Directives', [])
             customOnClick: '&dhClick',
             currentElement: '=dhCurrentElement',
             event: '=dhEvent',
-            id: '=dhId'
+            id: '=dhId',
+            field: '=dhField'
         },
         controller: [
             '$scope',
@@ -712,10 +713,15 @@ var d2Directives = angular.module('d2Directives', [])
 
                 $scope.valueClicked = function (buttonValue){
 
+                    if($scope.field) {
+                        $scope.field.$setDirty();
+                    }
+
                     $scope.clickedButton = buttonValue;
 
                     var originalValue = $scope.value;
                     var tempValue = buttonValue;
+
                     if($scope.value === buttonValue){
                         tempValue = "";
                     }
